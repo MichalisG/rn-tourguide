613b1ba2a36480ecec91493f189c2d155a9c6fb3
Michael G <michalis3489@gmail.com>
2021-02-24 19:12:56 +0200

    feat: push /lib

---
+++ lib/components/Tooltip.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+import { IStep, Labels } from '../types';
+export interface TooltipProps {
+    isFirstStep?: boolean;
+    isLastStep?: boolean;
+    currentStep: IStep;
+    stepsNumber?: number;
+    labels?: Labels;
+    handleNext?(): void;
+    handlePrev?(): void;
+    handleStop?(): void;
+}
+export declare const Tooltip: ({ isFirstStep, isLastStep, handleNext, handlePrev, handleStop, currentStep, labels, }: TooltipProps) => JSX.Element;
